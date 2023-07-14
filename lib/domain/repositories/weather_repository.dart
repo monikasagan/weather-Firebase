e5f3d1eb19276f81c12e3ec1d2b5a85035d87fe6
Monika Sagan <msaganx@gmail.com>
2023-07-14 23:40:08 +0200

    create weather mocked repository

---
+++ lib/domain/repositories/weather_repository.dart
@@ -0,0 +1,11 @@
+
+import 'package:weather/domain/models/weather_model.dart';
+
+
+class WeatherRepository {
+  Future<WeatherModel?> getWeatherModel({
+    required String city,
+  }) async {
+    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+  }
+}
