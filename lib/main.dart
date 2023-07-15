9ce094be55e1cce3f30e02ce2c8e9d8d28ea5320
Monika Sagan <msaganx@gmail.com>
2023-07-15 23:11:32 +0200

    Use material 13 and use dark theme data

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark(useMaterial3: true),
       home: const HomePage(),
     );
   }
