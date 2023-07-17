c36ccd1ef0f7ee5356a6f56a6da9f913c24e1d3b
Monika Sagan <msaganx@gmail.com>
2023-07-17 10:47:31 +0200

    Implement freezed in state

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,19 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:weather/app/core/enums.dart';
 import 'package:weather/domain/models/weather_model.dart';
 import 'package:weather/domain/repositories/weather_repository.dart';
 
 part 'home_state.dart';
+part 'home_cubit.freezed.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit(this._weatherRepository) : super(HomeState());
+  HomeCubit(this._weatherRepository) : super(const HomeState());
 
   final WeatherRepository _weatherRepository;
 
   Future<void> getWeatherModel({
     required String city,
   }) async {
-    emit(HomeState(status: Status.loading));
+    emit(const HomeState(status: Status.loading));
     try {
       final weatherModel = await _weatherRepository.getWeatherModel(city: city);
       emit(
